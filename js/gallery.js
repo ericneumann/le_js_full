c2dde20683ad3f42a297bfb17b634e430e7eae1d
Eric Neumann <ericn@hotmail.com>
2021-02-07 07:57:12 +0800

    Completed gallery app

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,31 @@
 function activateGallery() {
-  alert(document.querySelector("h1").textContent);
-}
+  // Class tags
+  let currentClass    = "current";
+  let titleClass      = "title";
+  let descClass       = "description";
+
+  let thumbnails      = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage       = document.querySelector("#gallery-photo > img");
+  let mainImageTitle  = document.querySelector(`#gallery-info > .${titleClass}`);
+  let mainImageDesc   = document.querySelector(`#gallery-info > .${descClass}`);
+  let galleryInfo     = document.querySelector(`#gallery-info`);
+
+  thumbnails.forEach(thumbnail => {
+    
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as the main image.
+      mainImage.setAttribute("src", thumbnail.dataset.largeVersion);
+      mainImage.setAttribute("alt", thumbnail.alt);
+
+      // Change which thumbnail image is current.
+      document.querySelector(`#gallery-thumbs > .${currentClass}`).classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+      
+      // Update gallery aside content
+      mainImageTitle.textContent = thumbnail.dataset.title;
+      mainImageDesc.textContent = thumbnail.dataset.description;
+
+    });
+
+  });
+};
